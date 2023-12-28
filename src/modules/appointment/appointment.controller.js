a802ee70dcb7c6843eb1e42f9a27fad7ed75b4ad
sondosammar3 <sondosammar18@gmail.com>
2023-12-29 00:53:33 +0200

    createAppointment

--- src/modules/appointment/appointment.controller.js
+++ src/modules/appointment/appointment.controller.js
@@ -1,46 +1,17 @@
+import { json } from "express";
 import appointmentModel from "../../../DB/model/appointment.model.js";
 import doctorModel from "../../../DB/model/doctor.model.js";
 import moment from 'moment'
 //user
-export const createAppointment=async(req,res,next)=>{
+export const createAppointment = async (req, res, next) => {
     const doctorId = req.params.doctorId;
     const { appointmentDate, appointmentTime, reason } = req.body;
-  
     const startOfDay = moment.utc(appointmentDate).startOf('day');
-const endOfDay = moment.utc(appointmentDate).endOf('day');
-const doctor=await doctorModel.findOne({ _id: doctorId,})
-
-const testHours = doctor.availability.filter(ele => ele.Date == Date(appointmentDate))// 
-return res.status(200).json(doctor.availability);
-        const availabilityDate = await doctorModel.findOne(
-            {
-                _id: doctorId,
-                'availability.Date': {
-                    $gte: startOfDay.toDate(),
-                    $lt: endOfDay.toDate()
-                }
-            },
-         
-        )
-        return res.json(doctor)
-        
-        
-        
-        if (!doctor) {
-            return next(new Error("Doctor not found", { status: 400 }));
-        }
-        
-        const currentDate = moment(appointmentDate).startOf('day');
-        const existingAppointment = await appointmentModel.findOne({
-            doctorId,
-            patientId: req.user._id,
-            appointmentDate,
-            reason
-        });
-
-        if (existingAppointment) {
-            return res.status(400).json({ message: "Appointment already exists for this day" });
-        }
+    const endOfDay = moment.utc(appointmentDate).endOf('day');
+    const doctor = await doctorModel.findOne({ _id: doctorId, })
+    const test = doctor.availability.filter((ele => ele.Date <= endOfDay && ele.Date >= startOfDay))[0]
+    let houer = test.availabilityHouer.includes(appointmentTime)
+    if (houer) {
 
         const appointment = await appointmentModel.create({
             doctorId,
@@ -49,13 +20,20 @@ return res.status(200).json(doctor.availability);
             appointmentTime,
             reason,
         });
+        const index = test.availabilityHouer.indexOf(appointmentTime);
+        if (index !== -1) {
+            test.availabilityHouer.splice(index, 1);
+            await doctor.save();
 
-        return res.status(201).json({ message: "Appointment created", appointment });
+        }
+       
+    }
+ return res.status(200).json(doctor);
 }
 
 //doctor
 //update by appointment_id 
-export const updateStatus=async(req,res,next)=>{
+export const updateStatus = async (req, res, next) => {
     const { appointment_id } = req.params;
     const { status } = req.body;
     const appointment = await appointmentModel.findOneAndUpdate(
@@ -86,9 +64,9 @@ export const reviewAppointmentScheduled = async (req, res, next) => {
 }
 
 //doctor
-export const reviewAllAppointment=async (req,res,next)=>{
+export const reviewAllAppointment = async (req, res, next) => {
     const doctorId = req.user._id;
-    const appointments = await appointmentModel.find({ doctorId}).select('-_id -createdAt -updatedAt -doctorId ').populate({
+    const appointments = await appointmentModel.find({ doctorId }).select('-_id -createdAt -updatedAt -doctorId ').populate({
         path: 'patientId',
         select: '_id  userName email phone address',
     });
@@ -100,42 +78,44 @@ export const reviewAllAppointment=async (req,res,next)=>{
 }
 
 //user
-export const GetAllAppointments_Patient=async(req,res,next)=>{
-    const patientId=req.user._id
+export const GetAllAppointments_Patient = async (req, res, next) => {
+    const patientId = req.user._id
     const appointments = await appointmentModel.find({ patientId }).select(' -createdAt -updatedAt -doctorId -__v ').populate({
         path: 'patientId',
         select: '-_id  userName email ',
     });
 
-   
+
     return res.json(appointments)
 }
 
 //user
-export const Cancel_Appointment=async(req,res,next)=>{
-   const {appointmentId}=req.params
-   const appointment=await appointmentModel.findByIdAndUpdate(appointmentId,{status:"Cancelled"},{new:true})
-   if (!appointment) {
-    return next(new Error("appointments not cancel", { cause: 400 }));
-}
-return res.json(appointment)
+export const Cancel_Appointment = async (req, res, next) => {
+    const { appointmentId } = req.params
+    const appointment = await appointmentModel.findByIdAndUpdate(appointmentId, { status: "Cancelled" }, { new: true })
+    if (!appointment) {
+        return next(new Error("appointments not cancel", { cause: 400 }));
+    }
+    return res.json(appointment)
 }
 //doctor
-export const appointmentsByDate=async(req,res,next)=>{
-const {date}=req.body
-const doctorId=req.user._id
-const startOfDay = moment.utc(date).startOf('day');
-const endOfDay = moment.utc(date).endOf('day');
-const appointment = await appointmentModel.find({doctorId
-    ,status:"Scheduled"
-,   appointmentDate: {
-    $gte: startOfDay.toDate(),
-    $lt: endOfDay.toDate()
-}}).select('-doctorId -appointmentDate -reason -status').populate('user'); // Select the appointment time for available slots
-if (!appointment) {
-    return next(new Error("appointments not cancel", { cause: 400 }));
-}
+export const appointmentsByDate = async (req, res, next) => {
+    const { date } = req.body
+    const doctorId = req.user._id
+    const startOfDay = moment.utc(date).startOf('day');
+    const endOfDay = moment.utc(date).endOf('day');
+    const appointment = await appointmentModel.find({
+        doctorId
+        , status: "Scheduled"
+        , appointmentDate: {
+            $gte: startOfDay.toDate(),
+            $lt: endOfDay.toDate()
+        }
+    }).select('-doctorId -appointmentDate -reason -status').populate('user'); // Select the appointment time for available slots
+    if (!appointment) {
+        return next(new Error("appointments not cancel", { cause: 400 }));
+    }
 
-return res.json(appointment)
+    return res.json(appointment)
 
 }
